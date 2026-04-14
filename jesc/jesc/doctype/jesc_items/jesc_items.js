87766d42da730ab5b08ffb0f2721d27d32057982
imadhubala27 <imadhubala27@gmail.com>
2026-04-14 10:45:12 +0530

    first commit

---
+++ jesc/jesc/doctype/jesc_items/jesc_items.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, emails@iwex.in and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("JESC Items", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ jesc/jesc/doctype/jesc_items/test_jesc_items.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, emails@iwex.in and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestJESCItems(FrappeTestCase):
+	pass
